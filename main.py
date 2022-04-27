cba9a97b43187b556107e30366e82e3ba523622a
Nayan <cnayan789@gmail.com>
2022-04-27 08:00:07 +0545

    validity function created

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-#initilized
+grid = [
+[4, 0, 0, 0, 0, 5, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 1, 9, 8],
+[3, 0, 0, 0, 8, 2, 4, 0, 0],
+[0, 0, 0, 1, 0, 0, 0, 8, 0],
+[9, 0, 3, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 3, 0, 6, 7, 0],
+[0, 5, 0, 0, 0, 9, 0, 0, 0],
+[0, 0, 0, 2, 0, 0, 9, 0, 7],
+[6, 4, 0, 3, 0, 0, 0, 0, 0],
+]
+
+def check_valid(row, col, num):
+  #check row
+  global grid
+  for i in range(9):
+      if grid[row][i] == num:
+          return False
+  #check column
+  for i in range(9):
+      if grid[i][col] == num:
+          return False
+  #check 3x3 grid
+  row_start = (row//3)*3
+  col_start = (col//3)*3
+  for i in range(row_start, row_start+3):
+      for j in range(col_start, col_start+3):
+          if grid[i][j] == num:
+              return False
+  return True
